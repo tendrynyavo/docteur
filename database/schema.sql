c7c7c383a038aa65c07e38ce281b863124459ac9
Tendrynyavo <143934345+tendrynyavo@users.noreply.github.com>
2024-01-12 20:21:45 +0300

    Update schema

--- database/schema.sql
+++ database/schema.sql
@@ -1,60 +1,48 @@
-CREATE TABLE client(
-   id_client VARCHAR(50) ,
+CREATE TABLE parametre(
+   id_parametre VARCHAR(50) ,
+   nom VARCHAR(100) ,
+   PRIMARY KEY(id_parametre)
+);
+
+CREATE TABLE patient(
+   id_patient VARCHAR(50) ,
    nom VARCHAR(100)  NOT NULL,
-   prenom VARCHAR(100)  NOT NULL,
-   PRIMARY KEY(id_client)
+   date_naissance DATE NOT NULL,
+   PRIMARY KEY(id_patient)
 );
 
-CREATE TABLE prestation(
-   id_prestation VARCHAR(50) ,
-   nom VARCHAR(100)  NOT NULL,
-   point INTEGER NOT NULL,
-   debut_etat INTEGER,
-   fin_etat INTEGER,
-   PRIMARY KEY(id_prestation),
-   UNIQUE(nom)
+CREATE TABLE medicament(
+   id_medicament VARCHAR(50) ,
+   nom VARCHAR(50)  NOT NULL,
+   prix DOUBLE PRECISION NOT NULL,
+   PRIMARY KEY(id_medicament)
 );
 
-CREATE TABLE dent(
-   numero VARCHAR(50) ,
-   priorite INTEGER,
-   PRIMARY KEY(numero)
+CREATE TABLE generation(
+   id_generation VARCHAR(50) ,
+   nom VARCHAR(50)  NOT NULL,
+   debut INTEGER NOT NULL,
+   fin INTEGER NOT NULL,
+   PRIMARY KEY(id_generation)
 );
 
-CREATE TABLE consultation(
-   id_consultation VARCHAR(50) ,
-   date_consultation DATE NOT NULL,
-   id_client VARCHAR(50)  NOT NULL,
-   PRIMARY KEY(id_consultation),
-   FOREIGN KEY(id_client) REFERENCES client(id_client)
+CREATE TABLE prescription(
+   id_parametre VARCHAR(50) ,
+   id_medicament VARCHAR(50) ,
+   effet DOUBLE PRECISION NOT NULL,
+   PRIMARY KEY(id_parametre, id_medicament),
+   FOREIGN KEY(id_parametre) REFERENCES parametre(id_parametre),
+   FOREIGN KEY(id_medicament) REFERENCES medicament(id_medicament)
 );
 
-CREATE TABLE traitement(
-   id_traitement VARCHAR(50) ,
-   etat INTEGER,
-   id_consultation VARCHAR(50)  NOT NULL,
-   numero VARCHAR(50)  NOT NULL,
-   id_prestation VARCHAR(50)  NOT NULL,
-   PRIMARY KEY(id_traitement),
-   FOREIGN KEY(id_consultation) REFERENCES consultation(id_consultation),
-   FOREIGN KEY(numero) REFERENCES dent(numero),
-   FOREIGN KEY(id_prestation) REFERENCES prestation(id_prestation)
-);
-
-CREATE TABLE etat_dentaire(
-   id_client VARCHAR(50) ,
-   numero VARCHAR(50) ,
-   etat INTEGER,
-   PRIMARY KEY(id_client, numero),
-   FOREIGN KEY(id_client) REFERENCES client(id_client),
-   FOREIGN KEY(numero) REFERENCES dent(numero)
-);
-
-CREATE TABLE cout(
-   id_prestation VARCHAR(50) ,
-   numero VARCHAR(50) ,
-   valeur DOUBLE PRECISION NOT NULL,
-   PRIMARY KEY(id_prestation, numero),
-   FOREIGN KEY(id_prestation) REFERENCES prestation(id_prestation),
-   FOREIGN KEY(numero) REFERENCES dent(numero)
+CREATE TABLE symptome(
+   id_parametre VARCHAR(50) ,
+   id_medicament VARCHAR(50) ,
+   id_generation VARCHAR(50) ,
+   debut INTEGER NOT NULL,
+   fin INTEGER NOT NULL,
+   PRIMARY KEY(id_parametre, id_medicament, id_generation),
+   FOREIGN KEY(id_parametre) REFERENCES parametre(id_parametre),
+   FOREIGN KEY(id_medicament) REFERENCES medicament(id_medicament),
+   FOREIGN KEY(id_generation) REFERENCES generation(id_generation)
 );
